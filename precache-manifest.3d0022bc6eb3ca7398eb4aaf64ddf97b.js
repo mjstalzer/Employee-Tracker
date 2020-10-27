e101071ff102a964faed595047c09bf18534012d
Matt Stalzer <mjs58181@uga.edu>
2020-10-27 13:07:09 -0400

    Updates

---
+++ precache-manifest.3d0022bc6eb3ca7398eb4aaf64ddf97b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce2373cb1b96d2caeb54ecffba31ffc",
+    "url": "/Employee-Tracker/index.html"
+  },
+  {
+    "revision": "deaf1b49153cd14c7c5f",
+    "url": "/Employee-Tracker/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9330e26437f87cae25b9",
+    "url": "/Employee-Tracker/static/css/main.03e5ab87.chunk.css"
+  },
+  {
+    "revision": "deaf1b49153cd14c7c5f",
+    "url": "/Employee-Tracker/static/js/2.a45b781f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Employee-Tracker/static/js/2.a45b781f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9330e26437f87cae25b9",
+    "url": "/Employee-Tracker/static/js/main.7bd5c973.chunk.js"
+  },
+  {
+    "revision": "8d2da1ffe8e7991a84de",
+    "url": "/Employee-Tracker/static/js/runtime-main.5366c593.js"
+  }
+]);
